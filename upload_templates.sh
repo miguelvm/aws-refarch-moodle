6d9542b66e9005beb331fdee86c2069be3e63f26
Miguel A. Vilarino <migav@amazon.com>
2020-03-15 13:17:58 +0100

    upload templates to stack name prefix

--- upload_templates.sh
+++ upload_templates.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+STACK_NAME="moodle34"
 for TEMPLATE in 00-master.yaml 01-newvpc.yaml 02-securitygroups.yaml 03-bastion.yaml 03-efsfilesystem.yaml 03-elasticache.yaml 03-publicalb.yaml 03-rds.yaml 04-cloudfront.yaml 04-efsalarms.yaml 04-web.yaml 05-route53.yaml
 do
-    aws s3 cp templates/$TEMPLATE s3://mb3moodle/templates/$TEMPLATE 
+    aws s3 cp templates/$TEMPLATE s3://mb3moodle/$STACK_NAME/templates/$TEMPLATE 
 done
 
 # Creates stack without session caching
@@ -9,11 +10,11 @@ done
 #moodle381.veiver.com
 #moodle39dev.veiver.com
 
-aws cloudformation create-stack --stack-name moodle34 --template-url https://s3-eu-west-1.amazonaws.com/mb3moodle/templates/00-master.yaml --parameters \
+aws cloudformation create-stack --stack-name moodle34 --template-url https://s3-eu-west-1.amazonaws.com/mb3moodle/$STACK_NAME/templates/00-master.yaml --parameters \
                     ParameterKey=DatabaseInstanceType,ParameterValue=db.r4.4xlarge \
                     ParameterKey=UseApplicationCacheBoolean,ParameterValue=true \
                     ParameterKey=EfsEncrpytedBoolean,ParameterValue=true \
-                    ParameterKey=DomainName,ParameterValue=moodle34.veiver.com \
+                    ParameterKey=DomainName,ParameterValue=$STACK_NAME.veiver.com \
                     ParameterKey=UseCloudFrontBoolean,ParameterValue=true \
                     ParameterKey=DatabaseName,ParameterValue=moodle \
                     ParameterKey=WebAsgMin,ParameterValue=1 \
@@ -29,6 +30,7 @@ aws cloudformation create-stack --stack-name moodle34 --template-url https://s3-
                     ParameterKey=DatabaseMasterPassword,ParameterValue=My0wnPr1vat3Pa33w0rd \
                     ParameterKey=DatabaseEncrpytedBoolean,ParameterValue=true \
                     ParameterKey=EfsPerformanceMode,ParameterValue=generalPurpose \
+                    ParameterKey=EfsGrowth,ParameterValue=256 \
                     ParameterKey=EC2KeyName,ParameterValue=moodle \
                     ParameterKey=HostedZoneName,ParameterValue=veiver.com \
                     ParameterKey=AvailabilityZones,ParameterValue=\"eu-west-1a,eu-west-1b\" \
