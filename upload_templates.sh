5232e12e33e323507e6345cc21a11666a2b0c745
Miguel A. Vilarino <migav@amazon.com>
2020-03-16 15:31:55 +0100

    updated instance types

--- upload_templates.sh
+++ upload_templates.sh
@@ -10,22 +10,23 @@ aws cloudformation create-stack --stack-name moodle39dev --template-url https://
                     ParameterKey=DatabaseInstanceType,ParameterValue=db.r5.4xlarge \
                     ParameterKey=UseApplicationCacheBoolean,ParameterValue=true \
                     ParameterKey=EfsEncrpytedBoolean,ParameterValue=true \
-                    ParameterKey=DomainName,ParameterValue=moodle39dev.veiver.com \
+                    ParameterKey=DomainName,ParameterValue=$STACK_NAME.veiver.com \
                     ParameterKey=UseCloudFrontBoolean,ParameterValue=true \
                     ParameterKey=DatabaseName,ParameterValue=moodle \
                     ParameterKey=WebAsgMin,ParameterValue=1 \
                     ParameterKey=WebAsgMax,ParameterValue=1 \
                     ParameterKey=PublicAlbAcmCertificate,ParameterValue=arn:aws:acm:eu-west-1:526038215496:certificate/5e24ec78-7951-4a59-acba-669df1d7ebf1 \
                     ParameterKey=MoodleLocale,ParameterValue=en \
-                    ParameterKey=ApplicationCacheNodeType,ParameterValue=cache.t2.micro \
+                    ParameterKey=ApplicationCacheNodeType,ParameterValue=cache.m5.xlarge \
                     ParameterKey=DatabaseMasterUsername,ParameterValue=moodle \
                     ParameterKey=NumberOfAZs,ParameterValue=2 \
                     ParameterKey=UseSessionCacheBoolean,ParameterValue=false \
-                    ParameterKey=WebInstanceType,ParameterValue=t3.xlarge \
-                    ParameterKey=SessionCacheNodeType,ParameterValue=cache.t2.micro \
+                    ParameterKey=WebInstanceType,ParameterValue=t3.2xlarge \
+                    ParameterKey=SessionCacheNodeType,ParameterValue=cache.m5.xlarge \
                     ParameterKey=DatabaseMasterPassword,ParameterValue=My0wnPr1vat3Pa33w0rd \
                     ParameterKey=DatabaseEncrpytedBoolean,ParameterValue=true \
                     ParameterKey=EfsPerformanceMode,ParameterValue=generalPurpose \
+                    ParameterKey=EfsGrowth,ParameterValue=256 \
                     ParameterKey=EC2KeyName,ParameterValue=moodle \
                     ParameterKey=HostedZoneName,ParameterValue=veiver.com \
                     ParameterKey=AvailabilityZones,ParameterValue=\"eu-west-1a,eu-west-1b\" \
@@ -36,7 +37,6 @@ aws cloudformation create-stack --stack-name moodle39dev --template-url https://
                     ParameterKey=EfsCmk,ParameterValue= \
                     ParameterKey=DatabaseCmk,ParameterValue= \
                     --capabilities CAPABILITY_NAMED_IAM --disable-rollback
-
 
 # Important: before running the configuration wizard
 # disable opcache in /etc/php-7.2.d/10-opcache.ini:
